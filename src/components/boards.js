59326ea685f3116dc4b69399478273c7c28f29b9
Vishal Pankaj <vishalpankaj9661@gmail.com>
2022-06-12 21:59:28 +0530

    first commit

---
+++ src/components/boards.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import './board.css';
+import List from './list'
+
+
+const Info = [
+    {
+        img : "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+        name : "Ayush Kumar Gupta", birthday : "2000-05-31"
+    },
+    {
+        img : "https://images.unsplash.com/photo-1503185912284-5271ff81b9a8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+        name : "Vishal Pankaj", birthday : "2001-08-03"
+    },
+    {
+        img : "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+        name : "Tara Kelly", birthday : "2000-04-16"
+    },
+    {
+      img : "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+        name : "Saurabh Baby", birthday : "2000-04-08"
+    },
+    {
+      img : "https://images.unsplash.com/photo-1503185912284-5271ff81b9a8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+        name : "Rohit ", birthday : "2000-01-10"
+    },
+    {
+      img : "https://images.unsplash.com/photo-1503185912284-5271ff81b9a8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+      name : "Ravinder ", birthday : "2000-06-07"
+    }
+  ]
+
+export default function board() {
+  return (
+      <main id='site-main'>
+          <h1 className="text-dark title">Birthday Remainder</h1>
+
+          <div className="board">
+                <List info={Today(Info)}></List>
+                <h2 className='upcoming text-dark'>Upcoming</h2>
+                <List info={Upcoming(Info, 2)} upcoming={true}></List>
+          </div>
+      </main>
+  );
+}
+
+function Today(person){
+    let currentDay = new Date().getDate();
+    let currentMonth = new Date().getMonth();
+
+    let filter = person.filter(data => {
+        let day = new Date(data.birthday).getDate();
+        let month = new Date(data.birthday).getMonth();
+
+        return currentDay === day && currentMonth === month;
+    })
+    return filter;
+}
+
+
+// upcoming birthdays
+function Upcoming(person, toMonth){
+    let currentMonth = new Date().getMonth();
+    let currentDay = new Date().getDate();
+
+    let filter =person.filter(data => {
+        let month = new Date(data.birthday).getMonth();
+        let day = new Date(data.birthday).getDate();
+
+        if (currentDay === day) return;
+        return month >= currentMonth && month <= currentMonth + toMonth;
+    })
+
+    return filter;
+
+}
